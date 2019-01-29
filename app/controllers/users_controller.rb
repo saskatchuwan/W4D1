efc9db9f6ba6403b2398af5cbc0d2ef7c59bf0b4
saskatchuwan <kathryn.chu10@gmail.com>
2019-01-28 16:37:57 -0800

    Initial commit for first routes and controller project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  def index
+    # render plain: "I'm in the index action!"
+    users = User.all
+    render json: users
+  end
+
+  def create
+    # render json: params 
+    user = User.new(user_params)
+    
+    #does user.save actually run? if only present in conditional statement
+    if user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+
+    render json: user 
+  end 
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user 
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end 
+  end 
+
+  def destroy
+    user = User.find(params[:id])
+
+    user.destroy
+
+    render json: user 
+  end 
+
+  private
+  def user_params
+    #does :user refer to our class?
+    params.require(:user).permit(:username, :email)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, :username, uniqueness: true, presence: true
+  
+end
